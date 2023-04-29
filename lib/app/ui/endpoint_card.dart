8d7865e2e6e2992fbffd806403ab08117cf47592
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-04-29 16:38:08 -0300

    Update the cards layout, image and color

--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -3,6 +3,18 @@
 import 'package:coronavirus_app/app/app.dart';
 import 'package:flutter/material.dart';
 
+class EndpointCardData {
+  final String title;
+  final String assetName;
+  final Color color;
+
+  EndpointCardData({
+    required this.title,
+    required this.assetName,
+    required this.color,
+  });
+}
+
 class EndpointCard extends StatelessWidget {
   final Endpoint endpoint;
   final int? value;
@@ -13,30 +25,66 @@ class EndpointCard extends StatelessWidget {
     required this.value,
   });
 
-  static final Map<Endpoint, String> _cardTitles = {
-    Endpoint.cases: 'Cases',
-    Endpoint.casesSuspected: 'Cases Suspected',
-    Endpoint.casesConfirmed: 'Cases Confirmed',
-    Endpoint.deaths: 'Deaths',
-    Endpoint.recovered: 'Recovered',
+  static final Map<Endpoint, EndpointCardData> _cardsData = {
+    Endpoint.cases: EndpointCardData(
+        title: 'Cases',
+        assetName: 'assets/count.png',
+        color: const Color(0xFFFFF492)),
+    Endpoint.casesSuspected: EndpointCardData(
+        title: 'Suspected Cases',
+        assetName: 'assets/suspect.png',
+        color: const Color(0xFFEEDA28)),
+    Endpoint.casesConfirmed: EndpointCardData(
+        title: 'Confirmed Cases',
+        assetName: 'assets/fever.png',
+        color: const Color(0xFFE99600)),
+    Endpoint.deaths: EndpointCardData(
+        title: 'Deaths',
+        assetName: 'assets/death.png',
+        color: const Color(0xFFE40000)),
+    Endpoint.recovered: EndpointCardData(
+        title: 'Recovered',
+        assetName: 'assets/patient.png',
+        color: const Color(0xFF70A901)),
   };
 
   @override
   Widget build(BuildContext context) {
+    final cardData = _cardsData[endpoint];
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       child: Card(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                _cardTitles[endpoint]!,
-                style: Theme.of(context).textTheme.headlineSmall,
+                cardData!.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5!
+                    .copyWith(color: cardData.color),
               ),
-              Text(value != null ? value.toString() : '',
-                  style: Theme.of(context).textTheme.titleLarge)
+              const SizedBox(height: 8),
+              SizedBox(
+                height: 52,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(cardData.assetName, color: cardData.color),
+                    Text(value != null ? value.toString() : '',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(
+                                color: cardData.color,
+                                fontWeight: FontWeight.w500))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
