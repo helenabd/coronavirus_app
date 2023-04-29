f58f8f175bf44aac071458db6fcd4a41392e590f
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-04-29 14:19:33 -0300

    Adding a Provider for the DataRepository

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,15 +1,33 @@
+// ignore_for_file: unnecessary_null_comparison, prefer_conditional_assignment, depend_on_referenced_packages
+
 import 'package:coronavirus_app/app/app.dart';
+import 'package:http/http.dart';
 
 class DataRepository {
   DataRepository({required this.apiService});
 
   final APIService apiService;
 
+  late String _accessToken;
+
   Future<int> getEndpointData({required Endpoint endpoint}) async {
-    final accessToken = await apiService.getAccessToken();
-    return await apiService.getEndpointData(
-      accessToken: accessToken,
-      endpoint: endpoint,
-    );
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+        accessToken: _accessToken,
+        endpoint: endpoint,
+      );
+    } on Response catch (response) {
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+          accessToken: _accessToken,
+          endpoint: endpoint,
+        );
+      }
+      rethrow;
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:coronavirus_app/app/services/api.dart';
-import 'package:coronavirus_app/app/services/api_service.dart';
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:coronavirus_app/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Provider<DataRepository>(
+      create: (_) => DataRepository(apiService: APIService(API.sandbox())),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Coronavirus Tracker',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: const Color(0xFF101010),
+          cardColor: const Color(0xFF222222),
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
