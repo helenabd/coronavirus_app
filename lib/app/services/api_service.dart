777ec9343131c46ac0e1ff83dea7eaed776f2623
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-04-29 12:27:49 -0300

    Requesting and parsing data from endpoints

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -27,4 +27,34 @@ class APIService {
     log('Request ${api.tokenUri()} failed \nResponse: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndpointData(
+      {required accessToken, required Endpoint endpoint}) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http.get(
+      uri,
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = await json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint]!;
+        final int result = endpointData[responseJsonKey];
+        return result;
+      }
+    }
+    log('Request $uri failed \nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  // Create a map that associate each endpoint to the json response
+  static final Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
 }
