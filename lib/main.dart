0f449fb3cf202497d4f39f7459b4806f0950e72e
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-04-29 21:32:35 -0300

    Injecting the DataCacheService in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,33 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'pt-BR';
   await initializeDateFormatting();
-  runApp(const MyApp());
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(MyApp(
+    sharedPreferences: sharedPreferences,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.sharedPreferences});
+
+  final SharedPreferences sharedPreferences;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Provider<DataRepository>(
       create: (_) => DataRepository(
-          apiService: APIService(API.sandbox()),
-          dataCacheServices:
-              DataCacheServices(sharedPreferences: sharedPreferences)),
+        apiService: APIService(API.sandbox()),
+        dataCacheServices: DataCacheServices(
+          sharedPreferences: sharedPreferences,
+        ),
+      ),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Coronavirus Tracker',
